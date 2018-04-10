867af26104842eba9d047207fff22dd225725d5e
JunchunDai <daijunchun2675@126.com>
2018-04-10 17:10:56 +0800

    整体结构

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $('#box').fullpage({
+    navigation: true,
+    css3: true,
+    // verticalCentered: ture,
+    scrollingSpeed: 800,
+    // menu: false,
+    anchors: ['homePage', 'CompanyProfile', 'productDescription', 'joinCooperation', 'feedBack'],
+    // menu: '#box',
+    // loopBottom: true,
+    // scrollOverflow: true,
+
+    sectionsColor: ['#ccc', '#4BBFC3', '#aadd', '#f90', '#ccc', '#fff'],
+    afterLoad: function (anchorLink, index) {
+
+    }
+  });
+
+
+
+
+
+
+})
